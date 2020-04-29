4b1c669e8266edb4ba3e977e4ac147ae1b01b576
oem <Oem@OEM>
2020-04-29 13:43:32 +0100

    added black and pre-commit

--- app/tesco_store_location_scraper.py
+++ app/tesco_store_location_scraper.py
@@ -8,17 +8,16 @@ from lxml import html
 import logging
 
 
-class TescoLocationScraper():
-
+class TescoLocationScraper:
     def __init__(self):
         logging.getLogger("urllib3").setLevel(logging.WARNING)
         self.invalidStoreID = []
         self.stores = []
-        self.url = 'http://www.tesco.com/store-locator/uk/?bID={}'
+        self.url = "http://www.tesco.com/store-locator/uk/?bID={}"
 
     def getURL(self, url):
         http = urllib3.PoolManager()
-        self.response = http.request('GET', url)
+        self.response = http.request("GET", url)
 
     def getStorLocation(self, url, storeID):
         details = {}
@@ -28,10 +27,10 @@ class TescoLocationScraper():
         error = xpatheval('.//*[@class="error"]')
         if len(error) != 0:
             return False
-        details['storeName'] = xpatheval('.//*[@class="store-name"]')[0].text
-        details['address'] = xpatheval('.//*[@itemprop="streetAddress"]')[0].text
-        details['telephone'] = xpatheval('.//*[@itemprop="telephone"]')[0].text
-        details['storeID'] = str(storeID)
+        details["storeName"] = xpatheval('.//*[@class="store-name"]')[0].text
+        details["address"] = xpatheval('.//*[@itemprop="streetAddress"]')[0].text
+        details["telephone"] = xpatheval('.//*[@itemprop="telephone"]')[0].text
+        details["storeID"] = str(storeID)
         self.stores.append(details)
         return True
 
@@ -40,35 +39,40 @@ class TescoLocationScraper():
             details = {}
             formatted_url = self.url.format(str(start))
             if not self.getStorLocation(formatted_url, start):
-                details['storeID'] = start
+                details["storeID"] = start
                 self.invalidStoreID.append(details)
             start = start + 1
         return self.stores, self.invalidStoreID
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(sys.argv)
     if len(sys.argv) != 3:
-        print('Invalid number of parameters:')
-        print('Enter storeID Range eg. ')
-        print('python tesco_store_location_scraper.py 3038 3045')
+        print("Invalid number of parameters:")
+        print("Enter storeID Range eg. ")
+        print("python tesco_store_location_scraper.py 3038 3045")
         exit(1)
     start = int(sys.argv[1])
     finish = int(sys.argv[2])
     if start > finish:
-        print('Invalid range : start cannot be greater than end')
+        print("Invalid range : start cannot be greater than end")
     scraper = TescoLocationScraper()
     stores, invalidStores = scraper.iterate_through_tesco_store_pages(start, finish)
-    print('Valid Stores:')
-    print('|Store ID     |Store Name      |Address      |Telephone |')
-    print('|---------|---------------------------------------|------------------'
-          '---------------------|---------------|')
+    print("Valid Stores:")
+    print("|Store ID     |Store Name      |Address      |Telephone |")
+    print(
+        "|---------|---------------------------------------|------------------"
+        "---------------------|---------------|"
+    )
     for store in stores:
-        print('|{0: <8}|{1: <40}|{2: <60}|{3: <15}'.format(
-              store['storeID'],
-              store['storeName'],
-              store['address'],
-              store['telephone']))
-    print('Invalid StoreIDs :')
+        print(
+            "|{0: <8}|{1: <40}|{2: <60}|{3: <15}".format(
+                store["storeID"],
+                store["storeName"],
+                store["address"],
+                store["telephone"],
+            )
+        )
+    print("Invalid StoreIDs :")
     for invalid_store in invalidStores:
-        print('{0: <8}'.format(invalid_store['storeID']))
+        print("{0: <8}".format(invalid_store["storeID"]))
