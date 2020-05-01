77a914a17901e65a9f3f49e14df172915e26913b
oem <Oem@OEM>
2020-05-01 16:48:58 +0100

    updated sql for store table create

--- scraper/scraper/stores.sql
+++ scraper/scraper/stores.sql
@@ -1,14 +1,18 @@
 create table stores
 (
-	storeID int auto_increment,
+    id int auto_increment
+	storeID int not null,
 	storeName varchar(200) not null,
 	address varchar(200) not null,
 	telephone varchar(40) null,
 	constraint stores_pk
-		primary key (storeID)
+		primary key (id)
 );
 
 create index stores_storeName_index
 	on stores (storeName desc);
+
+create index stores_storeName_index
+	on stores (storeID desc);
 
 commit;
